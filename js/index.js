32b59fd8f991ac896eaa45013e77eb99f9d5f94a
Liubomyr Kokor <ljubik@ukr.net>
2016-12-06 21:11:30 +0000

    js base

---
+++ js/index.js
@@ -0,0 +1,32 @@
+// alert('hellou World');//модальне вікно якщо його не закрити то нічого далі не виконується
+// document.write("<h1>Text in html</h1>"); //вивід тексту
+// console.log("konsole in dev tools");//вивід на консоль
+
+// var name = prompt("Enter your name"); //ввід даних в рядок
+// document.write("<h1>Hellou "+name+"</h1>"); //вивід
+
+//  var year =  +prompt("Enter year");// "+"" рівносильно Number() - це числові дані
+//  var mounts = Number (prompt("Enter mounts"));
+//  var day = Number (prompt("Enter day"));
+
+//  var sum = nazva(year, mounts) + day;
+
+//  document.write(sum);
+
+// function nazva(a, b){ //nazva це назва функції до якої потім можна звертатись
+//     return a+b;
+// }
+
+// function getAge(age){
+//     if (age === 18){ // == пробує сконвертувати, === якщо типи не рівні то не повертає
+//     return 'Old man';
+//     }else {
+//         return 'child'
+//     }
+    
+// }
+// var age=Number (prompt("Enter your age"));
+// var result = getAge(age);
+// document.write(result);
+
+document.write(Math.sqrt(25));//видубувати квадратний корінь
